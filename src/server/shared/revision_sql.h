bf64ff89f40c52ca3f262a478657ccb37b5c048e
Myh <mihai.dumitru@gxgnet.ro>
2010-08-01 02:53:21 +0300

    [280] Pit of Saron mobs fixed (spawned).

--- src/server/shared/revision_sql.h
+++ src/server/shared/revision_sql.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_SQL_H__
 #define __REVISION_SQL_H__
  #define REVISION_DB_CHARACTERS "required_165_characters_armory_patch"
- #define REVISION_DB_WORLD "required_243_world_creature_transport"
+ #define REVISION_DB_WORLD "required_280_world_pit_of_saron"
  #define REVISION_DB_REALMD "required_31_auth_db_version"
 #endif // __REVISION_SQL_H__
